b5a986639cc4d4ebb92d673e3d38dd0bc10d0b43
Raymond Ho <chunkiat82@gmail.com>
2016-06-09 11:25:44 +0800

    Initial commit

---
+++ src/main/java/org/chunkiat82/app/App.java
@@ -0,0 +1,13 @@
+package org.chunkiat82.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
